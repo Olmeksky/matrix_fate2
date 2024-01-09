6bea9acdcd30109c7414568b3c199e6a04e0e3bc
Aleksander <ShuSashi@yandex.ru>
2024-01-10 02:20:05 +0300

    Update personal.js

--- js/personal.js
+++ js/personal.js
@@ -1,4 +1,12 @@
 $(window).on('load', function() {
     console.log('All assets are loaded');
-   alert('Dct ujnjdj'); 
+   alert('Dct ujnjdj');
+
+    console.log(name);
+    console.log(birth);
+    console.log('год '+year);
+    console.log('число мес '+month);
+    console.log('число дня '+ day);
+
+    
 });
