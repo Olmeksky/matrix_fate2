fd9b24325d3f60181f4ae65a38d8730fbfd57cc2
Aleksander <ShuSashi@yandex.ru>
2024-01-09 03:39:21 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<div class="raschet" id="ras">
+  <div class="mblock">
+    <form action="./matrix.html" method="get">
+      <input type="date" name="datBirst" id="db" placeholder="Дата вашего рождения"> <br>
+      <button id="db_bt" style="background-color: #C59A5E;">Раcсчитать</button>
+    </form>
+  </div>
+</div>
