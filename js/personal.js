a3b12f13536fb6a0fab56f7a56ef3000f283f969
Aleksander <ShuSashi@yandex.ru>
2024-01-11 23:03:52 +0300

    Update personal.js

--- js/personal.js
+++ js/personal.js
@@ -87,7 +87,7 @@ $(window).on('load', function() {
 	i = arcan22(a+d);
 	/*console.log(a);console.log(b);console.log(c);console.log(d);console.log(e);console.log(f);console.log(g);console.log(h);console.log(i);*/
 	// Устанавливаем значения A-I на схему
-	$('div.A').html(a);
+	$('.A').html(a);
 	$('div.B').html(b);
 	$('div.C').html(c);
 	$('div.D').html(d);
